a50ce81ad9bec8a71e75f290bc9e2621277ddf24
Wout <woutstevens@live.nl>
2022-01-25 15:35:21 +0100

    Changed to class structure

--- app.py
+++ app.py
@@ -3,26 +3,28 @@ from utils import RestApi
 from utils import Command
 
 
-def on_call(command, args):
-    args_str = ''
-    for key, val in args.items():
-        args_str += f'{key} {val} '
-    args_str = args_str.strip()
+class App:
+    def __init__(self):
+        self._api = RestApi()
 
-    if command == Command.CONVERT:
-        os.system(f'python convert.py {args_str}')
-    elif command == Command.GENERATE:
-        os.system(f'python generate_model_config.py {args_str}')
-    elif command == Command.TRAIN:
-        os.system(f'python train.py {args_str}')
-    elif command == Command.PREDICT:
-        os.system(f'python predict.py {args_str}')
+    def start(self):
+        self._api.run(callback_func=self.on_call)
 
+    def on_call(self, command, args):
+        args_str = ''
+        for key, val in args.items():
+            args_str += f'{key} {val} '
+        args_str = args_str.strip()
 
-def main():
-    api = RestApi()
-    api.run(callback_func=on_call)
+        if command == Command.CONVERT:
+            os.system(f'python convert.py {args_str}')
+        elif command == Command.GENERATE:
+            os.system(f'python generate_model_config.py {args_str}')
+        elif command == Command.TRAIN:
+            os.system(f'python train.py {args_str}')
+        elif command == Command.PREDICT:
+            os.system(f'python predict.py {args_str}')
 
 
 if __name__ == '__main__':
-    main()
+    App().start()
